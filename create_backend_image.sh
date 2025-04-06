f3f916e5481427f086f70603649a37abfa2afd56
dabourt <dabourt62@gmail.com>
2025-04-06 19:35:01 +0200

    For Test Pipeline

--- create_backend_image.sh
+++ create_backend_image.sh
@@ -9,7 +9,7 @@ IMAGE_NAME="daboortocker/library_website:$VERSION_TAG"
 # Build the Docker image
 docker build -t $IMAGE_NAME backend_server
 
-# Push the Docker image to Docker Hub
+# Push the Docker image to Docker HuB
 docker push $IMAGE_NAME
 
 echo "Docker image $IMAGE_NAME has been built and pushed to Docker Hub."
